2b0811a467e3ed88f94292eb9a009004105819d6
konglingpeng20061 <155039946+konglingpeng20061@users.noreply.github.com>
2026-06-22 16:05:24 +0800

    Add src\router\menu.js

---
+++ src/router/menu.js
@@ -0,0 +1,26 @@
+export const menuItems = [
+  {
+    path: '/dashboard',
+    label: '首页工作台',
+    shortLabel: '首页',
+    disabled: false
+  },
+  {
+    path: '/api-docs',
+    label: '接口文档',
+    shortLabel: '接口',
+    disabled: false
+  },
+  {
+    path: '/forbidden',
+    label: '403 页面',
+    shortLabel: '403',
+    disabled: false
+  },
+  {
+    path: '/customer',
+    label: '客户管理',
+    shortLabel: '客户',
+    disabled: true
+  }
+]
